a52843224027938ecf805fbfbd154d4c21f34af0
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2024-04-18 03:28:54 +0000

    initial commit de branch

---
+++ Parabola.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Parabola {
+    //y = ax 2 + bx + c
+    //la forma cuadrática y canónica de la cónica
+    //la matriz asociada a la forma cuadrática
+    //los valores propios
+    //los vectores propios
+    //la gráfica
+
+    //Coordenadas del vértice (h, k)
+    //Distancia focal (p)
+    //Dirección (horizontal o vertical)
+}
